d88fd990ed79139ad777a1602c742d92a34d6190
ValeriaShestakova <lerashestakova@yandex.ru>
2016-06-10 16:04:09 +0300

    Lab4 v2

--- src/synchronization/Synchronization.java
+++ src/synchronization/Synchronization.java
@@ -31,15 +31,6 @@ public class Synchronization {
         String d2 = sc.nextLine();
         args[0] = d1;
         args[1] = d2;
-        /*String t1 = "fout1.tmp";
-        String t2 = "fout2.tmp";
-        File f1 = new File(t1);
-        File f2 = new File(t2); 
-        
-        Set<FileProperties> dir1;
-        Set<FileProperties> dir2;
-        Set<FileProperties> currentdir1;
-        Set<FileProperties> currentdir2;*/
         Config conf = new Config("config.xml");
         
        if (args.length > 0 && args.length % 2 == 0) {
@@ -52,51 +43,7 @@ public class Synchronization {
             conf.loadFromXML();
             //conf.printAll();
         }
-        
-        /*if (f1.exists()) {
-            // Загрузить коллекцию из файла
-            dir1 = compare.loadFromBynaryFile(f1);
-        } else { 
-            dir1 = new TreeSet<>();
-            // Рекурсивный поиск
-            compare.scanDir(d1, dir1);          
-        } 
-        if (f2.exists()) {
-            // Загрузить коллекцию из файла
-            dir2 = compare.loadFromBynaryFile(f2);
-        } else { 
-            dir2 = new TreeSet<>();
-            // Рекурсивный поиск
-            compare.scanDir(d2, dir2);            
-        }  
-        currentdir1 = new TreeSet<>();
-        compare.scanDir(d1, currentdir1);  
-        currentdir2 = new TreeSet<>();
-        compare.scanDir(d2, currentdir2); */
-       /*for (FileProperties fi: currentdir1) {
-            System.out.println(fi.getPath()+" "+fi.getModifiedTime()+" "+fi.isDirectory());
-        } 
-        System.out.println("Second");
-        for (FileProperties fi: currentdir2) {
-            System.out.println(fi.getPath()+" "+fi.getModifiedTime()+" "+fi.isDirectory());
-        } */
-       /* Thread myThready = new Thread(new Runnable()
-        {
-            @Override
-            public void run(){
-               try {
-                        compare.Synchr(currentdir1, currentdir2, dir1, dir2, conf);
-                    } catch (IOException ex) {
-                        Logger.getLogger(Synchronization.class.getName()).log(Level.SEVERE, null, ex);
-                    }  
-            }
-        }); 
-        
-        f1.delete();
-        f2.delete();
-        compare.saveToBinaryFile(currentdir1, f1);
-        compare.saveToBinaryFile(currentdir2, f2); */
-        
+                
         //myThready.start();
         String host = conf.getProperty("host");
         int port = Integer.valueOf(conf.getProperty("port"));
